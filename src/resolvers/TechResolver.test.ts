1ca1aa49c1eb9a007e6b2dac10e9c26195313eef
keith alleman <keith.alleman@icloud.com>
2019-11-28 09:54:06 -0700

    Added some testing on TechResolver.test.ts

--- src/resolvers/TechResolver.test.ts
+++ src/resolvers/TechResolver.test.ts
@@ -1,28 +1,52 @@
-// import { Connection } from 'typeorm';
-// import { connect } from '../test-utils/testConnection';
-// import { gCall } from '../test-utils/gCall';
-// import { createSchema } from '../utils/createSchema';
-// import { GraphQLSchema } from 'graphql';
+import { Connection } from 'typeorm';
+import { connect } from '../test-utils/testConnection';
+import { gCall } from '../test-utils/gCall';
+import { createSchema } from '../utils/createSchema';
+import { GraphQLSchema } from 'graphql';
+import { createTechMutation } from '../test-utils/testMutations';
 
-// let conn: Connection;
-// let testSchema: GraphQLSchema;
+let conn: Connection;
+let testSchema: GraphQLSchema;
 
-// beforeAll(async () => {
-//     conn = await connect();
-//     testSchema = await createSchema();
-// });
-// afterAll(async () => {
-//     await conn.close();
-// });
+beforeAll(async () => {
+    conn = await connect();
+    testSchema = await createSchema();
+});
+afterAll(async () => {
+    await conn.close();
+});
 
-// describe('Tech Resolver', () => {
-//     /**
-//      * testing if we can create a tech
-//      */
-//     it('createTech creates a new tech', async () => {
-//         const response = await gCall({
-//             schema: testSchema,
-//             source:
-//         })
-//     })
-// })
+describe('Tech Resolver', () => {
+    /**
+     * testing if we can create a tech
+     */
+    it('createTech creates a new tech', async () => {
+        const response = await gCall({
+            schema: testSchema,
+            source: createTechMutation,
+            variableValues: {
+                data: 'Keith Alleman',
+            },
+        });
+        expect(response).toMatchObject({
+            data: {
+                createTech: {
+                    id: 1,
+                    name: 'Keith Alleman',
+                },
+            },
+        });
+    });
+    it('throws error', async () => {
+        const response = await gCall({
+            schema: testSchema,
+            source: createTechMutation,
+            variableValues: {
+                data: null,
+            },
+        });
+        if (response.errors) {
+            expect(response.errors.length).toBeGreaterThan(0);
+        }
+    });
+});
